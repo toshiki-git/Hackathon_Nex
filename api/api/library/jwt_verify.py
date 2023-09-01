bdf7a348714d818969c69d459ba1e5140dafd3d0
NKMAK <nakamuraakira18@gmail.com>
2023-09-02 00:34:20 +0900

    Test compatible 4
    Test compatible 4

--- api/api/library/jwt_verify.py
+++ api/api/library/jwt_verify.py
@@ -7,7 +7,7 @@ from api.settings import settings
 async def jwt_verify(
     jwt_token : str = Header(),
     user_dao: UserDAO = Depends(),
-) -> dict:
+) -> dict[str, int | str]:
     try:
         if jwt_token is None:
             raise  HTTPException(status_code=401, detail="Authorization header is missing")
@@ -18,7 +18,7 @@ async def jwt_verify(
             algorithms=[settings.token_algorithm],
         )
         user_id = payload["user_id"]
-        user = user_dao.get_user(user_id) 
+        user = await user_dao.get_user(user_id) 
         
         if user is None:
             raise HTTPException(status_code=401, detail="non existent user")
@@ -26,8 +26,7 @@ async def jwt_verify(
         if await user_dao.is_email_exists(email=payload["email"]):
             user = await user_dao.get_user_by_email(email=payload["email"])
             if user:
-                response_data = {"user_id": user.id}
-                return response_data
+                return {"user_id": user.id}
 
         else:
             raise HTTPException(status_code=401, detail="not found email")
@@ -35,3 +34,5 @@ async def jwt_verify(
         raise HTTPException(status_code=401, detail="Token has expired")
     except JWTError:
         raise HTTPException(status_code=401, detail="Invalid token")
+    
+    return  {"user_id": "user.id"}
--- api/api/web/api/auth/jwt/views.py
+++ api/api/web/api/auth/jwt/views.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter, Depends
 from api.library.jwt_verify import jwt_verify
+
 router = APIRouter()
 
 @router.post("/verify")
-async def jwt_login_check(result: dict = Depends(jwt_verify)) -> dict:
+async def jwt_login_check(result: dict[str, int | str] = Depends(jwt_verify)) -> dict[str, int | str]:
     return result
