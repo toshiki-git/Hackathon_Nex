5e254d29d003fa648a6dc25327b69ce9487be506
Toshiki Takakubo <219tosiki@gmail.com>
2023-09-05 23:00:55 +0900

    スタイルの変更

--- web/src/components/PostArea.tsx
+++ web/src/components/PostArea.tsx
@@ -25,28 +25,20 @@ const PostArea: React.FC = () => {
   };
 
   return (
-    <div className="bg-gray-800 p-4 rounded-md">
-      <div className="text-xl text-white mb-4">投稿</div>
-      <div className="flex">
-        <div className="flex-1">
-          <Textarea
-            minRows={1}
-            placeholder="#ハッシュタグ"
-            value={hashtags}
-            className="bg-gray-700 border border-gray-600 rounded-md p-1 mb-2 text-white placeholder-gray-400"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setHashtags(e.target.value)
-            }
-          />
-          <Textarea
-            placeholder="投稿内容"
-            value={content}
-            className="w-full bg-gray-700 border border-gray-600 rounded-md p-1 text-white placeholder-gray-400"
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setContent(e.target.value)
-            }
-          />
-        </div>
+    <div className="bg-overlay p-4 rounded-md">
+      <div className="text-xl text-foreground mb-4">投稿</div>
+      <div className="flex-1 rounded-md p-1 mb-2 text-foreground placeholder-focus">
+        <Textarea
+          minRows={1}
+          placeholder="#ハッシュタグ"
+          value={hashtags}
+          onChange={(e) => setHashtags(e.target.value)}
+        />
+        <Textarea
+          placeholder="投稿内容"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+        />
       </div>
       <div className="mt-3 ml-2 flex justify-between items-center">
         <div className="flex items-center">
@@ -56,23 +48,20 @@ const PostArea: React.FC = () => {
             color="warning"
             variant="faded"
             aria-label="画像をアップロード"
-            className="mr-2 bg-gray-700 hover:bg-gray-600"
           >
             <CameraIcon />
           </Button>
           {selectedImage && imagePreviewUrl && (
             <div className="flex">
               <img
-                className="mr-1"
+                className="ml-1 rounded-md"
                 src={imagePreviewUrl}
                 alt="Selected preview"
-                style={{
-                  width: "30px",
-                  height: "30px",
-                  objectFit: "cover",
-                }}
+                style={{ width: "30px", height: "30px", objectFit: "cover" }}
               />
-              <p className="text-white ml-1">{selectedImage.name}が選択されました。</p>
+              <p className="text-foreground ml-1">
+                {selectedImage.name}が選択されました。
+              </p>
             </div>
           )}
         </div>
@@ -81,7 +70,6 @@ const PostArea: React.FC = () => {
           投稿
         </Button>
       </div>
-
       <input
         ref={fileInputRef}
         type="file"
