f080343d26788418ea3fe3b000a9c1cc2a9ac9a2
Toshiki Takakubo <219tosiki@gmail.com>
2023-09-05 20:32:59 +0900

    投稿画像のプレビューの追加

--- web/src/components/PostArea.tsx
+++ web/src/components/PostArea.tsx
@@ -7,6 +7,7 @@ const PostArea: React.FC = () => {
   const [hashtags, setHashtags] = useState<string>("");
   const [selectedImage, setSelectedImage] = useState<File | null>(null);
   const fileInputRef = useRef<HTMLInputElement | null>(null);
+  const imagePreviewUrl = selectedImage ? URL.createObjectURL(selectedImage) : null;
 
   const handleImageUpload = () => {
     if (fileInputRef.current) {
@@ -14,8 +15,8 @@ const PostArea: React.FC = () => {
     }
   };
 
-  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const file = event.target.files ? event.target.files[0] : null;
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
     setSelectedImage(file);
   };
 
@@ -24,46 +25,63 @@ const PostArea: React.FC = () => {
   };
 
   return (
-    <div className="bg-gray-800 p-4 rounded-md shadow-md">
-      <h2 className="text-xl text-white mb-4">投稿</h2>
+    <div className="bg-gray-800 p-4 rounded-md">
+      <div className="text-xl text-white mb-4">投稿</div>
       <div className="flex">
-        <div className="mr-3"></div>
         <div className="flex-1">
           <Textarea
             minRows={1}
             placeholder="#ハッシュタグ"
             value={hashtags}
-            className="w-full bg-gray-700 border border-gray-600 rounded-md p-2 mb-2 text-white placeholder-gray-400"
-            onChange={(e) => setHashtags(e.target.value)}
+            className="bg-gray-700 border border-gray-600 rounded-md p-1 mb-2 text-white placeholder-gray-400"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setHashtags(e.target.value)
+            }
           />
           <Textarea
             placeholder="投稿内容"
             value={content}
-            className="w-full bg-gray-700 border border-gray-600 rounded-md p-2 text-white placeholder-gray-400"
-            onChange={(e) => setContent(e.target.value)}
+            className="w-full bg-gray-700 border border-gray-600 rounded-md p-1 text-white placeholder-gray-400"
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+              setContent(e.target.value)
+            }
           />
         </div>
       </div>
       <div className="mt-3 ml-2 flex justify-between items-center">
-        <Button
-          onClick={handleImageUpload}
-          isIconOnly
-          color="warning"
-          variant="faded"
-          aria-label="画像をアップロード"
-          className="mr-2 bg-gray-700 hover:bg-gray-600"
-        >
-          <CameraIcon />
-        </Button>
-
-        {selectedImage && (
-          <p className="text-sm text-white">{selectedImage.name}が選択されました。</p>
-        )}
+        <div className="flex items-center">
+          <Button
+            onClick={handleImageUpload}
+            isIconOnly
+            color="warning"
+            variant="faded"
+            aria-label="画像をアップロード"
+            className="mr-2 bg-gray-700 hover:bg-gray-600"
+          >
+            <CameraIcon />
+          </Button>
+          {selectedImage && imagePreviewUrl && (
+            <div className="flex">
+              <img
+                className="mr-1"
+                src={imagePreviewUrl}
+                alt="Selected preview"
+                style={{
+                  width: "30px",
+                  height: "30px",
+                  objectFit: "cover",
+                }}
+              />
+              <p className="text-white ml-1">{selectedImage.name}が選択されました。</p>
+            </div>
+          )}
+        </div>
 
         <Button color="primary" onClick={handlePost}>
           投稿
         </Button>
       </div>
+
       <input
         ref={fileInputRef}
         type="file"
