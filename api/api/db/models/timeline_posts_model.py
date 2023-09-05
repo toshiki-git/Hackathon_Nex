b16ddedb1e7d1f933be1c47772e91ae6856cfbb9
NKMAK <nakamuraakira18@gmail.com>
2023-09-05 14:22:10 +0900

    Cooperation between GameTagModel and TimelinePostsModel
    Cooperation between GameTagModel and TimelinePostsModel

--- api/api/db/models/timeline_posts_model.py
+++ api/api/db/models/timeline_posts_model.py
@@ -1,10 +1,12 @@
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Column, Table
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DeclarativeBase
 from sqlalchemy import Integer, String
 
 from api.db.base import Base
 from api.db.models.user_model import UserModel
+
+from typing import List
 
 class TimelinePostsModel(Base):
     """Model for the timeline_posts table."""
@@ -19,4 +21,27 @@ class TimelinePostsModel(Base):
     )
     content : Mapped[str] = mapped_column(String(length=500), nullable=False)
     image_url : Mapped[str] = mapped_column(String(length=200), nullable=True)
+    
+    game_tags = relationship("GameTag", secondary="timeline_post_tag_association")
+    
+    
+class TimelineAssociationModel(DeclarativeBase):
+    pass
 
+timeline_post_tag_association = Table(
+    "timeline_post_tag_association",
+    Base.metadata,
+    Column("timeline_post_id", Integer, ForeignKey("timeline_posts.post_id"), primary_key=True),
+    Column("game_tag_id", Integer, ForeignKey("game_tag.id"), primary_key=True)
+)
+
+
+class GameTagModel(Base):
+    """Model for the game_tags table."""
+
+    __tablename__ = "game_tag"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    title: Mapped[str] = mapped_column(String, index=True)
+
+    timeline_posts: Mapped[List[TimelinePostsModel]] = relationship("TimelinePostsModel", secondary=timeline_post_tag_association, back_populates="game_tags")
