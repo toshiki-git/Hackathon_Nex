ab428100cf27204ce23bd6abcc63815616ddd548
HEKUCHAN <heitorhirose@gmail.com>
2023-09-07 02:45:01 +0900

    fix error

--- api/api/web/api/auth/views.py
+++ api/api/web/api/auth/views.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from api.web.api.auth import google, jwt
+from api.web.api.auth import google
 
 router = APIRouter()
 router.include_router(google.router, prefix="/google", tags=["GoogleAuth"])
