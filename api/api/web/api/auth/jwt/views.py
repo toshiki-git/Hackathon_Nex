d564d737c0c478c65280a544f542430b61767370
HEKUCHAN <heitorhirose@gmail.com>
2023-09-07 01:05:11 +0900

    #52 Ref

--- api/api/web/api/auth/jwt/views.py
+++ api/api/web/api/auth/jwt/views.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter, Depends
-from api.library.jwt_verify import jwt_verify
-from api.web.api.auth.jwt.schema import UserModelDTO
-from typing import List
+from api.library.is_authenticated import is_authenticated
+from api.web.api.users.schema import UserModelDTO
+
 router = APIRouter()
 
 # NOTE: この関数はテスト用関数で、削除予定
-@router.post("/verify", response_model = UserModelDTO)
-async def jwt_login_check(result : UserModelDTO  = Depends(jwt_verify)) -> UserModelDTO :
+@router.get("/verify", response_model = UserModelDTO)
+async def jwt_login_check(result : UserModelDTO  = Depends(is_authenticated)) -> UserModelDTO :
     return result
