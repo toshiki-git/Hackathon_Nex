544a6a151e4dd359204871ca677fa4eba9d5e7ee
HEKUCHAN <heitorhirose@gmail.com>
2023-09-07 01:48:36 +0900

    fix router

--- api/api/web/api/auth/views.py
+++ api/api/web/api/auth/views.py
@@ -4,4 +4,3 @@ from api.web.api.auth import google, jwt
 
 router = APIRouter()
 router.include_router(google.router, prefix="/google", tags=["GoogleAuth"])
-router.include_router(jwt.router, prefix="/jwt", tags=["jwtAuth"])
