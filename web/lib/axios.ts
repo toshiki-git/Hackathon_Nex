632a4b97285e45b1026390d71831abef21521950
HEKUCHAN <heitorhirose@gmail.com>
2023-09-07 07:36:00 +0900

    Conf axios

--- web/lib/axios.ts
+++ web/lib/axios.ts
@@ -1,36 +1,16 @@
-/* eslint no-param-reassign: "error" */
 import axios from "axios";
 
 const instance = axios.create();
 
-export interface ErrorResponse {
-  detail: string;
-}
-
-export interface Token {
-  token: string;
-  refresh_token: string;
-}
-
-export const axiosClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_ENDPOINT,
-  headers: {
-    "Content-Type": "application/json",
+instance.interceptors.request.use(
+  (config) => {
+    // eslint-disable-next-line no-param-reassign
+    config.baseURL = process.env.NEXT_PUBLIC_API_ENDPOINT as string;
+    return config;
   },
-});
+  (error) =>
+    // エラー時の処理
+    Promise.reject(error),
+);
 
-export const axiosServer = axios.create({
-  baseURL: process.env.API_ENDPOINT,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-const axiosSelf = axios.create({
-  baseURL: process.env.HOST,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
-
-export default axiosSelf;
+export default instance;
