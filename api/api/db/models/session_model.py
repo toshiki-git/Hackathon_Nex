e7605f4ae79fef2f62cababed88fb853ebdf4775
HEKUCHAN <heitorhirose@gmail.com>
2023-09-08 04:47:48 +0900

    新しいセッション管理のモデル作成

--- api/api/db/models/session_model.py
+++ api/api/db/models/session_model.py
@@ -1,30 +1,36 @@
 from datetime import datetime, timedelta
 
 from sqlalchemy import ForeignKey
+from sqlalchemy.ext.hybrid import hybrid_method
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy.sql.sqltypes import String, Integer, Boolean
+from sqlalchemy.sql.sqltypes import Boolean, Integer, String
 
 from api.db.base import Base
-from api.static import static
 from api.db.models.user_model import UserModel
+from api.static import static
 
 
 class SessionModel(Base):
     """Model for store session data."""
 
-    __tablename__ = "dummy_model"
+    __tablename__ = "sessions"
 
-    id = mapped_column(primary_key=True, autoincrement=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(
         Integer, ForeignKey(UserModel.id, onupdate="CASCADE", ondelete="CASCADE")
     )
-    session_cert = mapped_column(String(128))
-    refresh_token = mapped_column(String(128))
-    is_expired: Mapped[bool] = mapped_column(Boolean, default=False)
+    session_cert: Mapped[str] = mapped_column(String)
+    refresh_token: Mapped[str] = mapped_column(String(512))
+    is_discard: Mapped[bool] = mapped_column(Boolean, default=False)
 
-    def is_valid(self):
-        if self.is_expired:
+    @hybrid_method
+    def is_valid(self) -> bool:
+        if self.is_discard:
             return False
 
         expire_time = self.created_at + static.REFRESH_TOKEN_EXPIRE_TIME
         return datetime.now(static.TIME_ZONE) < expire_time
+
+    @hybrid_method
+    def discard(self):
+        self.is_discard = True
