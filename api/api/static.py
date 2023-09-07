cd1bd4ae3d0dea8f9b1cf2837a0477719e948638
HEKUCHAN <heitorhirose@gmail.com>
2023-09-08 04:46:01 +0900

    定数の命名をもう少しわかりやすくした

--- api/api/static.py
+++ api/api/static.py
@@ -8,8 +8,8 @@ class Static:
     GOOGLE_TOKEN_URL = "https://oauth2.googleapis.com/token"  # noqa: S105
     GOOGLE_AUTH_URL = "https://accounts.google.com/o/oauth2/auth"
     GOOGLE_USER_INFO_URL = "https://www.googleapis.com/oauth2/v1/userinfo"
-    KEY_TOKEN_EXPIRE_TIME = datetime.timedelta(minutes=15)
-    TOKEN_EXPIRE_TIME = datetime.timedelta(minutes=15)
+    MAGIC_LINK_EXPIRE_TIME = datetime.timedelta(minutes=15)
+    ACCESS_TOKEN_EXPIRE_TIME = datetime.timedelta(minutes=15)
     REFRESH_TOKEN_EXPIRE_TIME = datetime.timedelta(days=90)
     TIME_ZONE = ZoneInfo("Asia/Tokyo")
 
