93ec6a569ae8dc0eb04753b97be9588b84e9d042
HEKUCHAN <heitorhirose@gmail.com>
2023-09-09 23:26:29 +0900

    Delete LoginRequired

--- web/src/pages/notification/index.tsx
+++ web/src/pages/notification/index.tsx
@@ -1,11 +1,9 @@
 import getLayout from "@/components/layouts/main";
-import LoginRequired from "@/components/utils/LoginRequired";
 import NotificationList from "@/components/layouts/main/Notification/NotificationList";
 import { BsBellFill } from "react-icons/bs";
 
 const Notification = () => (
   <>
-    <LoginRequired />
     <div className="flex items-center text-4xl font-bold text-foreground mt-6 mb-4">
       <BsBellFill className="mr-2" /> 通知
     </div>
--- web/src/pages/search/index.tsx
+++ web/src/pages/search/index.tsx
@@ -1,12 +1,6 @@
 import getLayout from "@/components/layouts/main";
-import LoginRequired from "@/components/utils/LoginRequired";
 
-const Search = () => (
-  <>
-    <LoginRequired />
-    <h1>Search</h1>
-  </>
-);
+const Search = () => <h1>Search</h1>;
 
 Search.getLayout = getLayout;
 
